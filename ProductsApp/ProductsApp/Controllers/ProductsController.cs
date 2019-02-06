d53b67b45e69dced4f59552829b1822a4d67c442
David Serrano <davidsegu9@gmail.com>
2019-02-06 15:11:20 -0600

    Mis estudios
    mi investigación acerca de .net

--- ProductsApp/ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -1,30 +1,27 @@
-﻿using Productos.Models;
+﻿using ProductsApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
-namespace Productos.Controllers
+namespace ProductsApp.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[]  {
-
-
+        Product[] products = new Product[]
+         {
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
             new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-
-
-        };
+         };
 
 
         public IEnumerable<Product> GetAllProducts()
         {
             return products;
         }
+
 
         public IHttpActionResult GetProduct(int id)
         {
@@ -35,14 +32,5 @@ namespace Productos.Controllers
             }
             return Ok(product);
         }
-
     }
-
-
-
-
-
-
-
 }
- 
